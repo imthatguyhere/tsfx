a694fc86bf113e4d525397013b3674d02cdacd99
Kyle Graham <kylegraham05@gmail.com>
2025-05-23 09:56:43 -0700

    feat(core): Create Game Object model

---
+++ packages/core/src/models/entity/GameObject.ts
@@ -0,0 +1,7 @@
+import { Entity } from './Entity';
+
+export class GameObject extends Entity {
+    constructor(handle: number) {
+        super(handle);
+    }
+}
--- packages/core/src/models/entity/index.ts
+++ packages/core/src/models/entity/index.ts
@@ -1,3 +1,4 @@
 export { Entity } from './Entity';
+export { GameObject } from './GameObject';
 export { Ped } from './Ped';
 export { Vehicle } from './Vehicle';

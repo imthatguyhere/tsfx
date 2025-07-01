1b01d24ac902d5b406580eca6642ca9e364b3501
Kyle Graham <kylegraham05@gmail.com>
2025-06-30 20:34:02 -0700

    feat(hooks): Create development NUI event functions

--- packages/hooks/src/services/development.ts
+++ packages/hooks/src/services/development.ts
@@ -1,1 +1,23 @@
+import { NuiEvent } from '../providers';
+
 export const isDevBrowser = (): boolean => !(window as any).invokeNative;
+
+export function sendDevNuiEvent<T>(event: NuiEvent<T>, timeout: number = 1000): void {
+    if (!isDevBrowser) return;
+
+    setTimeout(() => {
+        window.dispatchEvent(
+            new MessageEvent('message', {
+                data: event
+            })
+        );
+    }, timeout);
+}
+
+export function sendDevNuiEvents<T>(events: NuiEvent<T>[], timeout: number = 1000): void {
+    if (!isDevBrowser) return;
+
+    for (const event of events) {
+        sendDevNuiEvent(event, timeout);
+    }
+}
